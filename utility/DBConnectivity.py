4504d40c2652ebaea24e21a62d8a109e6ed212fa
shamsid <shamshersiddiqui@hotmail.com>
2015-12-30 12:01:23 +0000

    Database modification

--- utility/DBConnectivity.py
+++ utility/DBConnectivity.py
@@ -5,7 +5,7 @@ Created on Oct 16, 2015
 '''
 import cx_Oracle
 def create_connection():
-    return cx_Oracle.Connection('T709988/T709988@10.123.79.56/GEORLI01')
+    return cx_Oracle.Connection('Database Connection Path')
 
 def create_cursor(con):
     return cx_Oracle.Cursor(con)
